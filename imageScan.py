50e507c5ca7ccb4bfa29b9c97acae0508ae2b118
Dr AlienTech <alientechgamig@hotmail.com>
2020-08-22 16:25:48 -0400

    removed blackspace so only text is saved to matrixN file, also fixed length check

--- imageScan.py
+++ imageScan.py
@@ -16,15 +16,15 @@ y1 = 934
 y2 = 949
 
 def snipecode():
+    startTime = time.time()
+    print("Start time: " + str(startTime))
+
     im = PIL.ImageGrab.grab(bbox=(x1, y1, x2, y2))
 
     # im.show()
 
     immatrix = np.array(im)
     immatrixT = immatrix.T[0]
-
-    print(np.shape(immatrix))
-    print(np.shape(immatrixT))
 
     thresholdIndices0 = immatrixT < 160
     immatrixT[thresholdIndices0] = 0
@@ -43,23 +43,45 @@ def snipecode():
     nonzero = np.where(score != 0)
     changes = np.where(np.diff(score))[0]+1
     immatrixTsplit = np.split(immatrixT, changes)
+    length = np.shape(immatrixTsplit)[0]
 
     print(score)
     print(zero)
     print(nonzero)
     print(changes)
-    print(np.shape(immatrixTsplit))
+    print(length)
 
-    file2write = open("matrix", 'w')
-    file2write.write(str(immatrixT))
-    file2write.close()
+    file0 = open("matrix2", 'w')
 
-    file2write = open("matrix1", 'w')
+    code = ""
+    
+    if length == 9:
+        immatrixTsplit = immatrixTsplit[1:8]
 
-    for i in range(9):
-        file2write.write(str(i) + "\n" + str(immatrixTsplit[i]) + "\n")
+    length = np.shape(immatrixTsplit)[0]
 
-    file2write.close()
+    if length != 7:
+        print("Error Code L" + str(length) +
+              ": Error occurred while extracting letters from image. Please contact Rasmit#2547 with a screenshot of the logs.")
+        return
+
+    for i in range(4):
+        code += read(immatrixTsplit[2 * i])
+        file0.write(str(i) + "\n" + str(immatrixTsplit[2 * i]) + "\n")
+
+    file0.close()
+
+    # file1 = open("matrix", 'w')
+    # file1.write(str(immatrixT))
+    # file1.close()
+
+    endTime = time.time()
+    print("End Time: " + str(endTime))
+
+    print("Elapsed Time: " + str(endTime - startTime) + " seconds")
+
+def read(matrix):
+    return "A"
 
 def main():
     snipecode()
