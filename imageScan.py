bd751a002a7b9ff6dc8a4ff9e688783040f10b4d
Dr AlienTech <alientechgamig@hotmail.com>
2020-09-01 00:07:40 -0400

    updated comparison iamges, padding, and testing images

--- imageScan.py
+++ imageScan.py
@@ -8,6 +8,7 @@ import string
 import time
 import numpy as np
 from numpy import save, load
+import math
 
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -64,17 +65,14 @@ def snipecode():
         # im.show()
         return
 
-    temp_prefix = str(startTime)[-6:]
-    os.mkdir('./tests/' + temp_prefix)
-
     # temp_prefix = "B9O2"
     # os.mkdir('./tests/' + temp_prefix)
 
     for i in range(4):
         code += read(immatrixTsplit[2 * i], i)
 
-        img = Image.fromarray(immatrixTsplit[2 * i]).save('./tests/' + temp_prefix + '/' + str(i) + '.png')
-        np.save('./tests/' + temp_prefix + '/' + str(i), immatrixTsplit[2 * i])
+        # img = Image.fromarray(immatrixTsplit[2 * i]).save('./tests/' + temp_prefix + '/' + str(i) + '.png')
+        # np.save('./tests/' + temp_prefix + '/' + str(i), immatrixTsplit[2 * i])
 
         # img = Image.fromarray(immatrixTsplit[2 * i]).save('./tests/' + temp_prefix + '/' + temp_prefix[i] + '.png')
         # np.save('./tests/' + temp_prefix + '/' + temp_prefix[i], immatrixTsplit[2 * i])
@@ -83,10 +81,18 @@ def snipecode():
 
     if ("I" in code or "1" in code):
         print("This ain't it chief")
+        # im.show()
     else:
         pyautogui.click(900, 950)
         pyautogui.typewrite('!claim ' + code)
         pyautogui.hotkey('enter')
+
+        # temp_prefix = str(startTime)[-6:]
+        # os.mkdir('./tests/' + temp_prefix)
+
+        # for i in range(4):
+        #     img = Image.fromarray(immatrixTsplit[2 * i]).save('./tests/' + temp_prefix + '/' + str(i) + '.png')
+        #     np.save('./tests/' + temp_prefix + '/' + str(i), immatrixTsplit[2 * i])
 
     endTime = time.time()
     print("Start time: " + str(startTime))
@@ -96,11 +102,8 @@ def snipecode():
 
 def read(matrix, index):
     if (index == 0 or index == 2):
-        # characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-        #           "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
         characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-                  "R", "S", "T", "U", "V", "W", "X", "Z"]
+                  "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     else:
         characters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
@@ -120,24 +123,52 @@ def read(matrix, index):
         if size0 != size1:
             if size0[0] > size1[0]:
                 if (size0[1] > size1[1]):  # make size10 bigger, make size11 bigger
-                    pad_size0 = (0, 0)
-                    pad_size1 = (size0[0] - size1[0], size0[1] - size1[1])  # size0 extrema
+                    l = size0[0] - size1[0]
+                    m = size0[1] - size1[1]
+
+                    a = math.ceil(l/2)
+                    b = math.ceil(m/2)
+                    
+                    pad_size0 = ((0,0), (0,0))
+                    pad_size1 = ((a, l - a), (b, m - b))  # size0 extrema
                 else:  # make size10 bigger, make size01 bigger
-                    pad_size0 = (0, size1[1] - size0[1])
-                    pad_size1 = (size0[0] - size1[0], 0)
+                    l = size1[1] - size0[1]
+                    m = size0[0] - size1[0]
+
+                    a = math.ceil(l/2)
+                    b = math.ceil(m/2)
+                    
+                    pad_size0 = ((0,0), (a, l - a))
+                    pad_size1 = ((b, m - b), (0,0))
             else:
                 if (size0[1] > size1[1]):  # make size00 bigger, make size11 bigger
-                    pad_size0 = (size1[0] - size0[0], 0)
-                    pad_size1 = (0, size0[1] - size1[1])
+                    l = size1[0] - size0[0]
+                    m = size0[1] - size1[1]
+
+                    a = math.ceil(l/2)
+                    b = math.ceil(m/2)
+                    
+                    pad_size0 = ((a, l - a), (0, 0))
+                    pad_size1 = ((0, 0), (b, m - b))
                 else:  # make size00 bigger, make size01 bigger
-                    pad_size0 = (size1[0] - size0[0], size1[1] - size0[1])  # size1 extrema
-                    pad_size1 = (0, 0)
+                    l = size1[0] - size0[0]
+                    m = size1[1] - size0[1]
+
+                    a = math.ceil(l/2)
+                    b = math.ceil(m/2)
+                    
+                    # size1 extrema
+                    pad_size0 = ((a, l - a), (b, m - b))
+                    pad_size1 = ((0, 0), (0, 0))
 
             # print(pad_size0)
             # print(pad_size1)
 
-            matrix = np.pad(matrix, pad_width=(pad_size0, (0, 0)), mode='constant', constant_values=0)
-            sampleQ = np.pad(sampleQ, pad_width=(pad_size1, (0, 0)), mode='constant', constant_values=0)
+            matrix = np.pad(matrix, pad_width=pad_size0, mode='constant', constant_values=0)
+            sampleQ = np.pad(sampleQ, pad_width=pad_size1, mode='constant', constant_values=0)
+
+            img0 = Image.fromarray(matrix).save('./tests/testing/' + character + str(index) + '.png')
+            img1 = Image.fromarray(sampleQ).save('./tests/testing/' + character + str(index) + 'control.png')
 
             size0 = np.shape(matrix)
             size1 = np.shape(sampleQ)
@@ -156,18 +187,8 @@ def read(matrix, index):
 
     return result
 
-def convert():
-    characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-        "R", "S", "T", "U", "V", "W", "X", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
-    for character in characters:
-        arr = np.load('./characters/' + character + '.npy')
-        img = Image.fromarray(arr.T)
-        img.save('./charactersPNG/' + character + '.png')
-
 def main():
-    # snipecode()
-    snipelist = ['05', '13', '20', '31', '39', '46', '54', '01']
+    snipelist = ['01', '09', '16', '24', '31', '39', '46', '54']
 
     while True:
         t = time.localtime()
@@ -186,5 +207,17 @@ def loop():
         time.sleep(2)
         snipecode()
 
-main()
+def convert():
+    characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+        "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+    for character in characters:
+        arr = np.load('./characters/' + character + '.npy')
+        img = Image.fromarray(arr.T)
+        img.save('./charactersPNG/' + character + '.png')
+
+
+snipecode()
+# main()
 # loop()
+# convert()
